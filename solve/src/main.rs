9f51476aefbce642510496e1caddb3f119bc8fd7
kamojiro <tamagoma002@yahoo.co.jp>
2021-06-01 23:36:17 +0900

    add lazy segment tree

--- solve/src/main.rs
+++ solve/src/main.rs
@@ -941,3 +941,133 @@ where
     // a*n+b = y*m+z
     return floor_sum(y, a, m, z);
 }
+
+// Lazy segment tree for range query and range update, alike problems
+// The closures must fulfill the defining laws of monoids
+// Indexing is 0-based
+// The code is based on the following web site.
+// https://algo-logic.info/segment-tree/
+#[derive(Clone, PartialEq, Debug)]
+struct LazySegmentTree<A, CUnit, CMult> {
+    data: Vec<A>,
+    lazy: Vec<A>,
+    monoid_unit_closure: CUnit,
+    monoid_op_closure: CMult,
+}
+
+#[allow(dead_code)]
+impl<A, CUnit, CMult> LazySegmentTree<A, CUnit, CMult>
+where
+    A: Copy + std::cmp::Eq,
+    CUnit: Fn() -> A,
+    CMult: Fn(A, A) -> A,
+{
+    fn new(n: usize, monoid_unit_closure: CUnit, monoid_op_closure: CMult) -> Self {
+        let mut nn = 1;
+        while nn < n {
+            nn *= 2;
+        }
+        let this = Self {
+            data: vec![monoid_unit_closure(); 2 * nn - 1],
+            lazy: vec![monoid_unit_closure(); 2 * nn - 1],
+            monoid_unit_closure,
+            monoid_op_closure,
+        };
+        return this;
+    }
+
+    fn from_slice(sl: &[A], monoid_unit_closure: CUnit, monoid_op_closure: CMult) -> Self {
+        let n = sl.len();
+        let mut nn = 1;
+        while nn < n {
+            nn *= 2;
+        }
+        let mut data = vec![monoid_unit_closure(); 2 * nn - 1];
+        for k in 0..n {
+            data[k + nn - 1] = sl[k];
+        }
+        if n >= 2 {
+            for j in (0..=(n + nn - 3) / 2).rev() {
+                data[j] = (monoid_op_closure)(data[j * 2 + 1], data[j * 2 + 2]);
+            }
+        }
+        let lazy = vec![monoid_unit_closure(); 2 * nn - 1];
+        Self {
+            data,
+            lazy,
+            monoid_unit_closure,
+            monoid_op_closure,
+        }
+    }
+
+    fn eval(&mut self, k: usize) {
+        if self.lazy[k] == (self.monoid_unit_closure)() {
+            return;
+        }
+        let n = (self.lazy.len() + 1) / 2;
+        if k < n - 1 {
+            self.lazy[k * 2 + 1] = self.lazy[k];
+            self.lazy[k * 2 + 2] = self.lazy[k];
+        }
+        self.data[k] = self.lazy[k];
+        self.lazy[k] = (self.monoid_unit_closure)();
+    }
+
+    fn sub_update(&mut self, a: usize, b: usize, x: A, k: usize, l: usize, r: usize) {
+        self.eval(k);
+        if a <= l && r <= b {
+            self.lazy[k] = x;
+            self.eval(k);
+        } else if a < r && l < b {
+            self.sub_update(a, b, x, k * 2 + 1, l, (l + r) / 2);
+            self.sub_update(a, b, x, k * 2 + 2, (l + r) / 2, r);
+            self.data[k] = (self.monoid_op_closure)(self.data[k * 2 + 1], self.data[k * 2 + 2]);
+        }
+    }
+
+    fn update_internal(&mut self, a: usize, b: usize, x: A) {
+        let n = (self.lazy.len() + 1) / 2;
+        self.sub_update(a, b, x, 0, 0, n)
+    }
+
+    fn sub_query_internal(&mut self, a: usize, b: usize, k: usize, l: usize, r: usize) -> A {
+        self.eval(k);
+        if r <= a || b <= l {
+            (self.monoid_unit_closure)()
+        } else if a <= l && r <= b {
+            self.data[k]
+        } else {
+            let vl = self.sub_query_internal(a, b, k * 2 + 1, l, (l + r) / 2);
+            let vr = self.sub_query_internal(a, b, k * 2 + 2, (l + r) / 2, r);
+            (self.monoid_op_closure)(vl, vr)
+        }
+    }
+
+    fn query_internal(&mut self, a: usize, b: usize) -> A {
+        let n = (self.lazy.len() + 1) / 2;
+        self.sub_query_internal(a, b, 0, 0, n)
+    }
+}
+
+trait LazyRangeQuery<T> {
+    type Output;
+    fn query(&mut self, r: T) -> Self::Output;
+    fn update(&mut self, r: T, x: Self::Output);
+}
+
+#[allow(dead_code)]
+impl<A, CUnit, CMult> LazyRangeQuery<std::ops::Range<usize>> for LazySegmentTree<A, CUnit, CMult>
+where
+    A: Copy + std::cmp::Eq,
+    CUnit: Fn() -> A,
+    CMult: Fn(A, A) -> A,
+{
+    type Output = A;
+    fn query(&mut self, range: std::ops::Range<usize>) -> A {
+        return self.query_internal(range.start, range.end);
+    }
+
+    fn update(&mut self, range: std::ops::Range<usize>, x: A) {
+        self.update_internal(range.start, range.end, x);
+    }
+}
